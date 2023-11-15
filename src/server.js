855917636486cbf462a949913813cc3c1c8e3731
CuongTran <trancuong.02092000@gmail.com>
2023-11-15 15:53:27 +0700

    feat: init code

--- src/server.js
+++ src/server.js
@@ -4,29 +4,42 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const imageFolderPath = '/Users/trancuong/Downloads';
+const mediaFolderPath = '/Users/trancuong/Downloads';
 
-// Serve images directly if requested
-app.use('/images', express.static(imageFolderPath));
+// Serve media files directly if requested
+app.use('/media', express.static(mediaFolderPath));
 
-// Generate and serve an HTML page with image links
-app.get('/', (req, res) => {
-    fs.readdir(imageFolderPath, (err, files) => {
-        if (err) {
-            res.status(500).send('Error reading the folder');
-            return;
+// Function to recursively read through folders
+function readDirectory(dir, fileList = []) {
+    fs.readdirSync(dir).forEach(file => {
+        const filePath = path.join(dir, file);
+
+        if (fs.statSync(filePath).isDirectory()) {
+            readDirectory(filePath, fileList);
+        } else {
+            fileList.push(filePath);
         }
-
-        let html = '<h1>Image Gallery</h1>';
-        files.forEach(file => {
-          // check if file is image
-          if (path.extname(file) === '.jpg' || path.extname(file) === '.jpeg' || path.extname(file) === '.png') {
-            html += `<img src="/images/${file}" style="max-width: 200px; max-height: 200px; margin: 10px;">`;
-          }
-        });
-
-        res.send(html);
     });
+    return fileList;
+}
+
+// Generate and serve an HTML page with media links
+app.get('/', (req, res) => {
+    const allFiles = readDirectory(mediaFolderPath);
+
+    let html = '<h1>Media Gallery</h1>';
+    allFiles.forEach(file => {
+        const relativeFilePath = path.relative(mediaFolderPath, file);
+        const fileUrl = `/media/${relativeFilePath.split(path.sep).join('/')}`;
+
+        if (file.match(/\.(jpeg|jpg|gif|png)$/)) {
+            html += `<img src="${fileUrl}" style="max-width: 200px; max-height: 200px; margin: 10px;">`;
+        } else if (file.match(/\.(mp4|webm)$/)) {
+            html += `<video src="${fileUrl}" controls style="max-width: 320px; max-height: 240px; margin: 10px;"></video>`;
+        }
+    });
+
+    res.send(html);
 });
 
 app.listen(port, () => {
